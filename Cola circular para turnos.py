18a6a78b5bfd8c073cdd0597293c53ecdd21aac0
l24590386-ship-it <l24590386@sjuanrio.tecnm.mx>
2025-10-18 18:40:39 -0600

    Add files via upload

---
+++ Cola circular para turnos.py
@@ -0,0 +1,28 @@
+#INSTITUTO TECNOLOGICO DE SAN JUAN DEL RIO 
+#ESTRUCTURA DE DATOS 
+#UNIDAD 1
+#NOMBRE : ROCIO MOLINA MONROY 
+#-----------------------------------------------------------
+
+class ColaCircular:
+    def __init__(self, jugadores):
+        self.jugadores = jugadores
+        self.indice = 0
+
+    def siguiente_turno(self):
+        jugador = self.jugadores[self.indice]
+        self.indice = (self.indice + 1) % len(self.jugadores)
+        return jugador
+
+
+def juego_turnos():
+    jugadores = input("Ingresa los nombres de los jugadores separados por coma: ").split(",")
+    jugadores = [j.strip() for j in jugadores]
+    cola = ColaCircular(jugadores)
+
+    while True:
+        input("\nPresiona ENTER para siguiente turno (o escribe 'salir' para terminar)...")
+        print(f"Turno de: {cola.siguiente_turno()}")
+
+
+juego_turnos()
